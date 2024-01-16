e612b4c0829930569998297da21ed43900ff7ca4
Anyla Kabashi <anyla.kabashi@gmail.com>
2024-01-16 19:58:12 +0100

    Added more JS

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-let person = prompt("Please enter your name", "Harry Potter")
+function sayMyName() {
+  let person = prompt("Please enter your name", "Harry Potter")
+  let text;
+  if (person == null || person == "") {
+    text = "User didn't want a personalized hello. Hello user! How are you today?";
+  } else if (person === "Harry Potter") {
+    text = "User is lazy. Hello lazy user! How are you today?"
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("pershendetje").innerHTML = text;
+}
+sayMyName();
