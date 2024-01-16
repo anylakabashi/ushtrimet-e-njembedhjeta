d7a41b915117644f5fa8bcd1926acdf326a63b72
Anyla Kabashi <anyla.kabashi@gmail.com>
2024-01-16 21:04:57 +0100

    Reviewed changes

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-function sayMyName() {
-  let person = prompt("Please enter your name", "Harry Potter")
-  let text;
-  if (person == null || person == "") {
-    text = "User didn't want a personalized hello. Hello user! How are you today?";
-  } else if (person === "Harry Potter") {
-    text = "User is lazy. Hello lazy user! How are you today?"
-  } else {
-    text = "Hello " + person + "! How are you today?";
-  }
-  document.getElementById("pershendetje").innerHTML = text;
-}
-sayMyName();
+// function sayMyName() {
+//   let person = prompt("Please enter your name", "Harry Potter")
+//   let text;
+//   if (person == null || person == "") {
+//     text = "User didn't want a personalized hello. Hello user! How are you today?";
+//   } else if (person === "Harry Potter") {
+//     text = "User is lazy. Hello lazy user! How are you today?"
+//   } else {
+//     text = "Hello " + person + "! How are you today?";
+//   }
+//   document.getElementById("pershendetje").innerHTML = text;
+// }
+// sayMyName();
 
 let texti = {
-  title: "Titulli ma i miri",
-  firstParagraph: "Cka ka cka ska?",
+  title: '',
+  firstParagraph: '',
   ndrrojeTitullin: function (newTitle) {
-    document.getElementById('titulli').innerHTML = newTitle
+    this.title = newTitle
+    document.getElementById('titulli').innerHTML = this.title
   },
   ndrrojeParagrafin: function (newContent) {
-    document.getElementById('paragrafiPare').innerHTML = newContent
+    this.firstParagraph = newContent
+    document.getElementById('paragrafiPare').innerHTML = this.firstParagraph
   }
 }
 texti.ndrrojeParagrafin("test");
